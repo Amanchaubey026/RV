779e9337442c87c6bbdce1d37d5257757a3a80ea
Aman Chaubey <98681520+Amanchaubey026@users.noreply.github.com>
2024-10-31 06:34:46 +0000

    auth0

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { Auth0Provider } from '@auth0/auth0-react'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-af2d0st1an0prlul.us.auth0.com"
+      clientId="IX7Wrz568ya7kf4Sp9ylXoADAY2RKq5D"
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+    >
+      <App />
+    </Auth0Provider>
   </StrictMode>,
 )
